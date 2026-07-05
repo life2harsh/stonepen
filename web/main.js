a4db6b9903da96b6009b50d8d9d7496f7e06ca30
Stonepen <stonepen@local>
2026-07-05 18:34:12 +0530

    keep Stonepen runtime alive for page lifetime

--- web/main.js
+++ web/main.js
@@ -1,6 +1,4 @@
 import init, { mount_stonepen } from "./pkg/stonepen_wasm.js";
 
 await init();
-const stonepen = mount_stonepen("ink-canvas");
-window.stonepen = stonepen;
-window.mount_stonepen = mount_stonepen;
+export const stonepen = mount_stonepen("ink-canvas");
